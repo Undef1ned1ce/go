eca58809fec6742266da1cbb02ca408797fe36bd
root <root@debian>
2021-01-12 14:32:00 +0100

    Premier commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+valeur:= flag.Bool("version",False,"Affiche") * bool
+ 
+const VERSION = "1.0"
+
+func main() {
+   fmt.Printf("Hello, World!\n")
+
+
+flag.Bool ("version",False,"Affiche" )
+flag.Parse ()
+
+
+}
